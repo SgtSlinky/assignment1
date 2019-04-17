af7a2946e90e4ca7b537df746112424432d2e4fa
Doug Mason <doug85au@hotmail.com>
2019-04-17 08:30:28 +0000

    encription working in main

--- assignment1.c
+++ assignment1.c
@@ -4,23 +4,34 @@
 
 int main(){
 
-char name[25] = {'d', 'o', 'u', 'g', 'l', 'a', 's'};
+char name[25] = {'D', 'O', 'U', 'G', 'L', 'a', 'S'};
 
-int key = 1;
+int key = 10;
 char encription[7];
 int size = 7;
+
+
+
 
 
 //char name[] = char name
 for(int i=0; i<size; i++){
     
-    encription[i] = name[i] - key;
+    if(name[i] >= 97 && name[i] <= 122){
+    name[i] = name[i] - 32;
+    }
+    if((name[i] >=0 && name[i]<=64) || (name[i] >= 91 && name[i] <= 96) || (name[i] >= 123 )){
+        encription[i] = name[i];
+    }
+    else{
+    encription[i] = name[i] + key;
     
-    
-    if(encription[i] < 97){
-       encription[i] = 122; //- (key--);
+        if(encription[i] > 90){
+        encription[i] = 65 + (encription[i] - 91);
+    //   printf("%c ", encription[i]);
     }  
-    
+}
+
     printf("%c ", encription[i]);
     
 
