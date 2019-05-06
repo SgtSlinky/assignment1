213f3384ee5e568c4cd9e672ca127fe1503267bc
SgtSlinky <48339637+SgtSlinky@users.noreply.github.com>
2019-05-06 14:54:47 +1000

    Update assignment1.c

--- assignment1.c
+++ assignment1.c
@@ -24,38 +24,37 @@ scanf ("%d", &menu);
 if (menu == 1){                                     //menu option 1 encription with rotation cipher of users choice
     char name[] = "Attack At Dawn!";                //message to be encoded
     size = sizeof (name);                           //size of message
-    int key;
+    int key;                                        
     char encription[size];
       
-    printf ("Enter the key ");
+    printf ("Enter the key ");                      //ask for encoding key
     scanf ("%d", &key);
       
         while (key < -25 || key > 25){
 	  
-            printf ("Key must be between -25 and 25. Enter a new key ");
+            printf ("Key must be between -25 and 25. Enter a new key ");        //only accept key between 
 	        scanf ("%d", &key);
 	        }
       
 
 	        for (int i = 0; i < size; i++){
 	  
-                if (name[i] >= 97 && name[i] <= 122){
+                if (name[i] >= 97 && name[i] <= 122){                       //convert lower case letters to upper case letters
 	                name[i] = name[i] - 32;
 	                }
 	  
-                if ((name[i] >= 0 && name[i] <= 64) || (name[i] >= 91 && name[i] <= 96) || (name[i] >= 123)){
-	      
-                    encription[i] = name[i];
+                if ((name[i] >= 0 && name[i] <= 64) || (name[i] >= 91 && name[i] <= 96) || (name[i] >= 123)){   //leave special charachters as they are
+	               encription[i] = name[i];
 	                }
 	  
 	            else{
-	                encription[i] = name[i] + key;
+	                encription[i] = name[i] + key;                             //apply encription key
 	      
  
-                if (encription[i] > 90){
-		            encription[i] = 65 + (encription[i] - 91);
-		            }
-        }
+                    if (encription[i] > 90){                                    // prevent letters from becoming characters 
+		                encription[i] = 65 + (encription[i] - 91);
+		                }
+                    }
 
     printf ("%c ", encription[i]);
 
@@ -67,7 +66,8 @@ return 0;
 }
   
 if (menu == 2){                                                         //menu option 2 decription 
-    char name[] = "IBBIKS IB LIEV!";                                    //IBBIKS IB LIEV!
+    char name[] = "IBBIKS IB LIEV!";                                    //8 letter rotation = IBBIKS IB LIEV!
+    size = sizeof (name);                                               //size of message
     int key;
     char encription[size];
     printf ("Enter the key ");
@@ -79,26 +79,23 @@ if (menu == 2){                                                         //menu o
 	    }
       
     for (int i = 0; i < size; i++){
-	    //if (name[i] >= 97 && name[i] <= 122){
-	    //    name[i] = name[i] - 32;
-	    //    }
-	  
-        if ((name[i] >= 0 && name[i] <= 64)|| (name[i] >= 91 && name[i] <= 96) || (name[i] >= 123)){
+	    
+        if ((name[i] >= 0 && name[i] <= 64)|| (name[i] >= 91 && name[i] <= 96) || (name[i] >= 123)){    //leave special characters as they are
             encription[i] = name[i];
 	        }
 	  
-    else{
-	    encription[i] = name[i] - key;
-    }  
-
+        else{
+	        encription[i] = name[i] - key;                                                                 //apply decription key
+            }  
+    
  
     if (encription[i] < 65){
-        encription[i] = 91 - 65 + (encription[i]);
+        encription[i] = 91 - 65 + (encription[i]);                                                      //loop alphabet so letters dont become characters
         }
-	    
+    
     
     printf ("%c ", encription[i]);
-	
+    
 }
       
 printf ("\n");
@@ -109,31 +106,6 @@ return 0;
  
 if (menu == 3){
 
-    #include <string.h>
-
-    
-        char* ciphertext = "qwertyuiopasdfghjklzxcvbnm";    // cipher lookup
-
-        char input[500];                                    // input buffer
-        printf("Enter text: ");
-        fgets(input, sizeof(input), stdin);                 // safe input from user
-        input[strlen(input) - 1] = 0;                       // remove the \n (newline)
-        int count = strlen(input);                          // get the string length
-
-        char output[count];                                 // output string
-        for(int i = 0; i < count; i++) {                    // loop through characters in input
-        int index = ((int) input[i]) - 97;                  // get the index in the cipher by subtracting 'a' (97) from the current character
-        if(index < 0) {
-            output[i] = ' ';                                // if index < 0, put a space to account for spaces
-        }
-        else {
-            output[i] = ciphertext[index];                  // else, assign the output[i] to the ciphertext[index]
-        }
-    }
-    output[count] = 0;                                      // null-terminate the string
-
-    printf("output: %s\n", output);                         // output the result
-
     
 }
   
@@ -141,7 +113,7 @@ if (menu == 3){
 if (menu == 4)
     {
       
-;
+printf("404 not found");
     
 }
   
@@ -149,7 +121,7 @@ if (menu == 4)
 if (menu == 5)
     {
       
-;
+printf("404 not found");
     
 }
   
@@ -157,7 +129,7 @@ if (menu == 5)
 if (menu == 6)
     {
       
-;
+printf("404 not found");
     
 }
   
@@ -175,6 +147,10 @@ return 0;
 
 
  
+
+
+
+
 
 
 
